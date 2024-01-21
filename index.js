113346923d568e3904d5a5dc3197df01f58b2145
zerodegress <zerodegress@outlook.com>
2024-01-21 17:06:56 +0800

    增加导出函数

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 import { tokenize } from './token.js'
 import { parse } from './parse.js'
-import { compile as compileToRWASM } from './compile.js'
-import {
-  compile as compileToRWIni,
-  optimize as optimizeRWASM,
-} from './rwasm.js'
-import {
-  compile as compileToString,
-  optimize as optimizeRWIni,
-} from './rwini.js'
+import { compile as compileNode } from './compile.js'
+import { compile as compileRWASM, optimize as optimizeRWASM } from './rwasm.js'
+import { compile as compileRWIni, optimize as optimizeRWIni } from './rwini.js'
+
+export {
+  tokenize,
+  parse,
+  compileNode,
+  compileRWASM,
+  compileRWIni,
+  optimizeRWASM,
+  optimizeRWIni,
+}
 
 /** @type {(src: string) => string} */
 export function compile(src) {
-  return compileToString(
+  return compileRWIni(
     optimizeRWIni(
-      compileToRWIni(optimizeRWASM(compileToRWASM(parse(tokenize(src))))),
+      compileRWASM(optimizeRWASM(compileNode(parse(tokenize(src))))),
     ),
   )
 }
